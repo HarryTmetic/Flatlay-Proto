a5aad18d65e1f8aace61199336fb3ab201bc0949
HarryTmetic <31948116+HarryTmetic@users.noreply.github.com>
2018-05-07 15:15:21 +0900

    Update likes.sol

--- likes.sol
+++ likes.sol
@@ -40,12 +40,29 @@ contract Likes is Owned {
         return likerAccts;
     }
     
-    function getLiker(address _address) view public returns (bytes16, bytes16) {
-        return (likes[_address].firstName, likes[_address].lastName);
+    function getLiker(address _address) view public returns (string, string) {
+        return (bytes32ToString(likes[_address].firstName), bytes32ToString(likes[_address].lastName));
     }
     
     function likesCount() view public returns (uint) {
         return likerAccts.length;
+    }
+    
+    function bytes32ToString(bytes32 x) private constant returns (string) {
+        bytes memory bytesString = new bytes(32);
+        uint charCount = 0;
+        for (uint j = 0; j < 32; j++) {
+            byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
+            if (char != 0) {
+                bytesString[charCount] = char;
+                charCount++;
+            }
+        }
+        bytes memory bytesStringTrimmed = new bytes(charCount);
+        for (j = 0; j < charCount; j++) {
+            bytesStringTrimmed[j] = bytesString[j];
+        }
+        return string(bytesStringTrimmed);
     }
     
 }
